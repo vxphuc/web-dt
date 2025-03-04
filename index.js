7d894fa2aa4416849168a131cc81f5de0f361bd6
Phungthevinh <ptvstar2003@gmail.com>
2025-03-04 19:37:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const db = require('./config/db/index');
 const cors = require('cors')
 require('dotenv').config()
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
