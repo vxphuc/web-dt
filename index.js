ed4a74f88c831331423255664ad0e3a8e6cb1950
Phungthevinh <ptvstar2003@gmail.com>
2025-04-16 08:30:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const router = require('./routers/index');
-const db = require('./config/db/index');
-const cors = require('cors')
-require('dotenv').config()
-const cookieParser = require('cookie-parser');
-
+const express = require("express");
+const app = express();
+const port = 5000;
+const router = require("./routers/index");
+const db = require("./config/db/index");
+const cors = require("cors");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "https://67f28886901b4b419e38b27f-teal-beigne.netlify.app", // CHỈ ĐỊNH chính xác FE
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "https://67f28886901b4b419e38b27f-teal-beigne.netlify.app",
+      "http://localhost:3000",
+    ], // CHỈ ĐỊNH chính xác FE
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 //router
 router(app);
@@ -25,7 +29,6 @@ router(app);
 //database
 db.connect();
 
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`);
+});
