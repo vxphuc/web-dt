4aaf5d5d5335c8a3fbe78860e813444bc8bf6be5
Phungthevinh <ptvstar2003@gmail.com>
2025-04-18 08:55:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;;
 const router = require("./routers/index");
 const db = require("./config/db/index");
 const cors = require("cors");
