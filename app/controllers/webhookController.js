256417da6f7a52c660e40b0676abae3abb5ce752
Phungthevinh <ptvstar2003@gmail.com>
2025-04-26 09:47:31 +0700

    Update webhookController.js

--- app/controllers/webhookController.js
+++ app/controllers/webhookController.js
@@ -1,10 +1,10 @@
 const ValidateSignature = require("../../utils/signatureValidator.js");
 
 const handleWebhook = async (req, res) => {
-  // const secureToken = req.headers[`secure-token`]
-  // if (!ValidateSignature.validateSignature(secureToken)) {
-  //   return res.status(403).json({ message: "Forbidden" });
-  // }
+  const secureToken = req.headers[`secure-token`]
+  if (!ValidateSignature.validateSignature(secureToken)) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
   console.log("Webhook received:", req.body);
   res.status(200).json({ message: "Webhook received successfully" });
 };
