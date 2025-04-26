b5678fe057f6d7beece31208026e26187f4e96f4
Phungthevinh <ptvstar2003@gmail.com>
2025-04-26 10:57:59 +0700

    update code

--- app/controllers/webhookController.js
+++ app/controllers/webhookController.js
@@ -2,8 +2,7 @@ const ValidateSignature = require("../../utils/signatureValidator.js");
 
 const handleWebhook = async (req, res) => {
   console.log("Headers:", req.headers)
-  const signature = req.headers['x-casso-signature'] || req.headers['X-Casso-Signature'];
-  if (!ValidateSignature.isValidCassoSignature(req.body, signature)) {
+  if (!ValidateSignature.isValidCassoSignature(req.body)) {
     return res.status(403).json({ message: "Forbidden" });
   }
   console.log("Webhook received:", req.body);
--- utils/signatureValidator.js
+++ utils/signatureValidator.js
@@ -1,20 +1,37 @@
 require(`dotenv`).config();
-const crypto = require('crypto');
+const crypto = require("crypto");
 
+const VALID_TOKEN = process.env.CASSO_SECURE_TOKEN;
 
-const VALID_TOKEN = process.env.CASSO_SECURE_TOKEN
+const isValidCassoSignature = (body) => {
+  const timestamp = Date.now().toString();
 
-const isValidCassoSignature = (body, signatureFromCasso) => {
-    const computedSignature  = crypto
-        .createHmac('sha256', VALID_TOKEN)
-        .update(JSON.stringify(body))
-        .digest('hex');
+  const payload = `${timestamp}.${JSON.stringify(body)}`;
 
-    return crypto.timingSafeEqual(
-        Buffer.from(signatureFromCasso, 'hex'),
-        Buffer.from(computedSignature, 'hex')
-    );
-}
+  const signature = crypto
+    .createHmac("sha256", VALID_TOKEN)
+    .update(payload)
+    .digest("hex");
 
-module.exports = { isValidCassoSignature }
+  const xCassoSignature = `t=${timestamp},v1=${signature}`;
 
+  // Gửi request bằng axios
+  axios
+    .post("http://localhost:8080/api/webhook-event-handler", body, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Casso-Signature": xCassoSignature,
+      },
+    })
+    .then((response) => {
+      console.log("✅ Server response:", response.data);
+    })
+    .catch((error) => {
+      console.error(
+        "❌ Error sending webhook:",
+        error.response?.data || error.message
+      );
+    });
+};
+
+module.exports = { isValidCassoSignature };
