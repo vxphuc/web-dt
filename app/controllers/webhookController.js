6193a251085f60f2a1f01fa1caa837bac315926f
Phungthevinh <ptvstar2003@gmail.com>
2025-04-26 14:56:52 +0700

    Update webhookController.js

--- app/controllers/webhookController.js
+++ app/controllers/webhookController.js
@@ -1,11 +1,11 @@
 
 const handleWebhook = async (req, res) => {
   console.log("Received webhook:", req.body);
-  console.log("Received webhook headers:", req.body.description.includes(req.body.orderId ));
+  console.log("Received webhook headers:", req.body.data.description.includes(req.body.orderId ));
   if(!req.body.orderId || !req.body.amount) {
     return res.status(400).json({ code: 400, message: "Bad Request" });
   }
-  if (req.body.description.includes(req.body.orderId ) && req.body.amount === req.body.amountBill) {
+  if (req.body.data.description.includes(req.body.orderId ) && req.body.data.amount === req.body.amountBill) {
     return res.status(200).json({ code: 200, message: "OK" });
   }
 };
