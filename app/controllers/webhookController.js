deaf160802fe8564fa47ed461630accd8b1d30be
Phungthevinh <ptvstar2003@gmail.com>
2025-04-26 14:35:26 +0700

    Update webhookController.js

--- app/controllers/webhookController.js
+++ app/controllers/webhookController.js
@@ -1,8 +1,9 @@
 
-
 const handleWebhook = async (req, res) => {
-
   console.log("Received webhook:", req.body);
+  if(!req.body.orderId || !req.body.amount) {
+    return res.status(400).json({ code: 400, message: "Bad Request" });
+  }
   res.status(200).json({ code: 200, message: "OK" });
 };
 
