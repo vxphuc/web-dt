7183a9d4e27b593850ecb70b8b20c78e312c413f
Phungthevinh <ptvstar2003@gmail.com>
2025-04-26 10:27:53 +0700

    Update webhookController.js

--- app/controllers/webhookController.js
+++ app/controllers/webhookController.js
@@ -2,7 +2,7 @@ const ValidateSignature = require("../../utils/signatureValidator.js");
 
 const handleWebhook = async (req, res) => {
   console.log("Headers:", req.headers)
-  const signature  = req.headers[`X-Casso-Signature`]
+  const signature = req.headers['x-casso-signature'] || req.headers['X-Casso-Signature'];
   if (!ValidateSignature.isValidCassoSignature(req.body, signature)) {
     return res.status(403).json({ message: "Forbidden" });
   }
