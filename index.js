2c813ff385cc6970c5f98005f4e222abe32add7f
Phungthevinh <ptvstar2003@gmail.com>
2025-04-26 14:04:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ app.use(
   })
 );
 
-// Middleware lưu lại raw body
+
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.use(express.json({
   verify: (req, res, buf) => {
     req.rawBody = buf.toString(); // <- bắt buộc để tạo signature chính xác
   }
 }));
-
-// app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 //router
 router(app);
