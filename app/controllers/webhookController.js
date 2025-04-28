27db31ab03d45b900564d6035ebd2e3cf161af7f
Phungthevinh <ptvstar2003@gmail.com>
2025-04-28 09:38:06 +0700

    Update webhookController.js

--- app/controllers/webhookController.js
+++ app/controllers/webhookController.js
@@ -3,16 +3,18 @@ const bills = require("../models/bill");
 const handleWebhook = async (req, res) => {
   try {
     console.log("Received webhook:", req.body);
+
     const description = req.body.data.description;
     const descriptionSplit = description.split(" ")[1];
     console.log("Description split:", descriptionSplit);
-    const bill = bills.findOne({ _id: req.body.id });
+
+    const bill = bills.findOne({ _id: descriptionSplit });
+    console.log("Bill found:", bill);
+
     return res.status(200).json({ code: 200, message: "OK" });
   } catch (error) {
     console.error("Error handling webhook:", error);
-    return res
-      .status(500)
-      .json({ code: 500, message: "Internal Server Error" });
+    return res.status(500).json({ code: 500, message: "Internal Server Error" });
   }
 };
 
