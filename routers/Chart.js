12c75dac158d7b11e881c30aef461b857628fda5
Phungthevinh <ptvstar2003@gmail.com>
2025-05-05 13:06:05 +0700

    Update Chart.js

--- routers/Chart.js
+++ routers/Chart.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const chartController = require("../app/controllers/chartController");
+const checkAuth = require("../app/middlewares/checkAuth");
+const checkRole = require("../app/middlewares/checkRole");
 
-router.get("/getYearRevenue", chartController.getYearRevenue);
+router.get("/getYearRevenue", checkAuth, checkRole(['admin']), chartController.getYearRevenue);
 
 
 module.exports = router;
