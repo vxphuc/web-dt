b9508cbbac2d4a92823277c70d60858ce2b52819
Phungthevinh <ptvstar2003@gmail.com>
2025-06-23 10:02:46 +0700

    Update worker.js

--- app/queue/worker.js
+++ app/queue/worker.js
@@ -5,7 +5,6 @@ const Notification = require("../models/Notification");
 
 const startWorker = async (server) => {
   const io = await initSocket(server);
-  const redisConn = createRedisClient();
   const worker = new Worker(
     "notificationQueue",
     async (job) => {
@@ -13,13 +12,17 @@ const startWorker = async (server) => {
       const n = await Notification.create({
         orderId,
         message,
-      },{
-        connection: redisConn,
       });
       io.to("admins").emit("notification", n);
     },
     {
-      connection: redisConn,
+      connection: {
+        password: "baCuPa8MxDKqjEA0YKYFEndn9sVeO2Fj",
+        socket: {
+          host: "redis-10304.c99.us-east-1-4.ec2.redns.redis-cloud.com",
+          port: 10304,
+        },
+      },
     }
   );
 };
