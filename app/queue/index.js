07372f486e06306c04d7dca576b87c1522a05790
Phungthevinh <ptvstar2003@gmail.com>
2025-06-23 11:22:46 +0700

    Update index.js

--- app/queue/index.js
+++ app/queue/index.js
@@ -4,10 +4,10 @@ const { createRedisClient } = require('../../config/redis');
 let queueInstance;
 
 async function getQueue() {
-  if (!queueInstance) {
+
     const redisClient = await createRedisClient();
     queueInstance = new Queue('notificationQueue', { connection: redisClient });
-  }
+
   return queueInstance;
 }
 
