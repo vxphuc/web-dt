e77ef510c13ec61118e88c42168b2011ec444b2e
Phungthevinh <ptvstar2003@gmail.com>
2025-07-02 16:55:14 +0700

    Update checkAuth.js

--- app/middlewares/checkAuth.js
+++ app/middlewares/checkAuth.js
@@ -1,23 +1,27 @@
+// middleware/auth.js (sửa lại)
 const User = require('../models/user');
 const admin = require('../../config/firebaseConfig');
 
 const checkAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) 
+    return res.status(401).json({ error: 'Unauthorized: No token provided' });
 
-  const token = req.cookies.authToken 
+  const parts = authHeader.split(' ');
+  if (parts.length !== 2 || parts[0] !== 'Bearer') 
+    return res.status(401).json({ error: 'Unauthorized: Bad token format' });
 
-  if (!token) return res.status(401).json({ error: "Unauthorized: No token provided" });
+  const token = parts[1];
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
     const user = await User.findOne({ uid: decodedToken.uid });
-
-    if (!user) return res.status(401).json({ error: "Unauthorized: User not found" });
+    if (!user) return res.status(401).json({ error: 'Unauthorized: User not found' });
 
     req.user = user;
     next();
-  } catch (error) { 
-    return res.status(401).json({ error: "Unauthorized: Invalid token" });
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthorized: Invalid token' });
   }
 };
-
 module.exports = checkAuth;
