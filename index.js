22438f7ac2cf94d95626e8b19784ce953482f60e
Phungthevinh <ptvstar2003@gmail.com>
2025-07-08 18:11:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const whitelist = [
   "http://localhost:3000",
   "https://sieuthidt.com",
   "http://localhost:2999",
-  "https://zalo.me/s/1780581978027324044/"
+  "https://zalo.me/s/1780581978027324044"
 ];
 
 // --- 2. Cấu hình cors động để trả đúng origin ---
