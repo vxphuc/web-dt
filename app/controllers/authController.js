e2ae54d600f34fc01a88f71afa5bc395490dcc7e
Phungthevinh <ptvstar2003@gmail.com>
2025-07-12 15:35:03 +0700

    Update authController.js

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -69,12 +69,6 @@ async function signin(req, res, next) {
       });
       await user.save();
     }
-
-    if (user) {
-      user.numberPhone = req.body.numberPhone;
-      await user.save();
-    }
-
     const isProduction = process.env.NODE_ENV === "production";
 
     // res.cookie("authToken", idToken, {
