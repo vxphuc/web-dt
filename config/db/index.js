dd36cc59e938f0c714950dd0f72bc0c2eb42788f
Phungthevinh <ptvstar2003@gmail.com>
2025-07-15 20:00:43 +0700

    Update index.js

--- config/db/index.js
+++ config/db/index.js
@@ -4,5 +4,6 @@ function connect() {
         .then(() => console.log('Connected!'));
 }
 // mongodb+srv://hkmediadtgroup:41iofkP4SrmA49wk@cluster0.3lrfdio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// mongodb+srv://hkmediadtgroup:hkmedia@dtshop.ryjmkoe.mongodb.net/?retryWrites=true&w=majority&appName=DTShop
 
 module.exports = { connect };
