29632c32a82c51489ec9941927d9b1d9071bb7c7
Phungthevinh <ptvstar2003@gmail.com>
2025-07-15 07:34:12 +0700

    Update index.js

--- config/db/index.js
+++ config/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function connect() {
-    mongoose.connect('mongodb+srv://hkmediadtgroup:hkmedia@dtshop.ryjmkoe.mongodb.net/?retryWrites=true&w=majority&appName=DTShop')
+    mongoose.connect('mongodb+srv://hkmediadtgroup:41iofkP4SrmA49wk@cluster0.3lrfdio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         .then(() => console.log('Connected!'));
 }
 
