49f894f74b42fec445fea556b3e4255b835b5713
Phungthevinh <ptvstar2003@gmail.com>
2025-08-07 11:19:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { connectRedis } = require("./config/redis");
     "http://localhost:3000",
     "https://sieuthidt.com",
     "http://localhost:2999",
-    "https://h5.zdn.vn"
+    "https://h5.zdn.vn",
   ];
 
   const corsOptions = {
@@ -32,8 +32,8 @@ const { connectRedis } = require("./config/redis");
 
   app.use(cookieParser());
   app.use(cors(corsOptions));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
   app.use(express.static("public"));
   app.options("*", cors(corsOptions));
 
@@ -49,5 +49,4 @@ const { connectRedis } = require("./config/redis");
   server.listen(port, () => {
     console.log(`🚀 App listening on port ${port}`);
   });
-
 })();
