a3ff6a698612e2bd0106309628adbf4ae572b59e
Phungthevinh <ptvstar2003@gmail.com>
2025-08-07 10:06:59 +0700

    Update otpService.js

--- services/otpService.js
+++ services/otpService.js
@@ -1,30 +1,35 @@
-const {getRedisClient} = require('../config/redis');
+const { getRedisClient } = require("../config/redis");
 
 const OTP_EXPIRE_TIME = 60 * 5; // 5 minutes
 
 const generateOtp = (length = 6) => {
-    const randomOTP = Math.floor(10 ** 5  + Math.random()*9 * 10 ** 5).toString();
-    return randomOTP
-}
+  const randomOTP = Math.floor(
+    10 ** 5 + Math.random() * 9 * 10 ** 5
+  ).toString();
+  return randomOTP;
+};
 
 const createOtp = async (phone) => {
-    const otp = generateOtp();
-    const redis = getRedisClient();
-    await redis.set(phone, otp, {EX: OTP_EXPIRE_TIME});
-    return otp;
-}
+  const otp = generateOtp();
+  const redis = getRedisClient();
+  await redis.set(phone, otp, {
+    EX: OTP_EXPIRE_TIME,
+  });
+
+  return otp;
+};
 
 const verifyOtp = async (phone, otp) => {
-    const redis = getRedisClient();
-    const storedOtp = await redis.get(phone);
-    if (storedOtp === otp) {
-        await redis.del(phone);
-        return true;
-    }
-    return false;
-}
+  const redis = getRedisClient();
+  const storedOtp = await redis.get(phone);
+  if (storedOtp === otp) {
+    await redis.del(phone);
+    return true;
+  }
+  return false;
+};
 
 module.exports = {
-    createOtp,
-    verifyOtp,
-}
+  createOtp,
+  verifyOtp,
+};
