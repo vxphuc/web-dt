2a8bdaf073471aa2a0ff40d1b5cd56a461219c60
Phungthevinh <ptvstar2003@gmail.com>
2025-12-01 10:40:53 +0700

    upload

--- app/controllers/webhookController.js
+++ app/controllers/webhookController.js
@@ -3,31 +3,35 @@ const bill = require("../models/bill");
 
 const handleWebhook = async (req, res) => {
   try {
-    console.log("Received webhook:", req.body);
-    const description = req.body.data.description;
-    // Regex tìm mã MongoDB 24 ký tự
-    const match = description.match(/[a-f0-9]{24}/i);
-    const orderId = match ? match[0] : null;
-    const Bank = new bank({
-      id: req.body.data.id,
-      transactionDateTime: req.body.data.transactionDateTime,
-      description,
-      orderId,
-      amount: req.body.data.amount,
-    });
+    // console.log("Received webhook:", req.body);
+    // const description = req.body.data.description;
+    // // Regex tìm mã MongoDB 24 ký tự
+    // const match = description.match(/[a-f0-9]{24}/i);
+    // const orderId = match ? match[0] : null;
+    // const Bank = new bank({
+    //   id: req.body.data.id,
+    //   transactionDateTime: req.body.data.transactionDateTime,
+    //   description,
+    //   orderId,
+    //   amount: req.body.data.amount,
+    // });
 
-    await Bank.save();
+    // await Bank.save();
 
-    return res.status(200).json({ code: 200, message: "OK", data: Bank });
+    // return res.status(200).json({ code: 200, message: "OK", data: Bank });
+
+    res.status(200).json({ message: "Hello world" });
   } catch (error) {
     console.error("Error handling webhook:", error);
-    return res.status(500).json({ code: 500, message: "Internal Server Error" });
+    return res
+      .status(500)
+      .json({ code: 500, message: "Internal Server Error" });
   }
 };
 
 const handlecheck = async (req, res) => {
   try {
-    const {id}  = req.body;
+    const { id } = req.body;
     const bankData = await bank.findOne({ orderId: id });
     if (!bankData) {
       console.log("Bank data not found for id:", id);
@@ -36,11 +40,31 @@ const handlecheck = async (req, res) => {
     return res.status(200).json({ code: 200, message: "OK", data: bankData });
   } catch (error) {
     console.error("Error handling check:", error);
-    return res.status(500).json({ code: 500, message: "Internal Server Error" });
+    return res
+      .status(500)
+      .json({ code: 500, message: "Internal Server Error" });
+  }
+};
+
+const usersPaid = async (req, res, next) => {
+  try {
+    res.status(200).json({ message: "Hello world" });
+  } catch {
+    res.status(500);
+  }
+};
+
+const registerWebhook = async (req, res, next) => {
+  try {
+    res.status(200).json({ message: "Hello world" });
+  } catch {
+    res.status(500);
   }
 };
 
 module.exports = {
   handleWebhook,
-  handlecheck
+  handlecheck,
+  usersPaid,
+  registerWebhook,
 };
--- routers/webhook.js
+++ routers/webhook.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const webhookController = require('../app/controllers/webhookController');
 
-router.post('/webhook', webhookController.handleWebhook);
+//Router này sẽ là webhook nhận thông tin giao dịch từ casso gọi qua được bảo mật bằng secure_token trong header
+router.post('/handler-bank-transfer', webhookController.handleWebhook);
+
+// Router này sẽ thực hiện tính năng đồng bộ giao dịch tức thì.
+// Ví dụ: Khi người dùng chuyển khoản cho bạn và họ ấn nút tôi đã thanh toán thì nên xử lí gọi qua casso đề đồng bộ giao dịch vừa chuyển khoản
+router.post("/users-paid", webhookController.usersPaid)
+
+// Route này sẽ thực hiện đăng kí webhook dựa vào API KEY và lấy thông tin về business và banks
+router.post('/register-webhook', webhookController.registerWebhook)
+
 router.post('/check', webhookController.handlecheck);
 
 module.exports = router;
