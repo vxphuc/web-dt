bee23d1f9f7f2f7aef58cea8ce951e497dd1f816
Phungthevinh <ptvstar2003@gmail.com>
2025-12-01 09:10:51 +0700

    Update webhook.js

--- routers/webhook.js
+++ routers/webhook.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const webhookController = require('../app/controllers/webhookController');
 
-router.get('/webhook', webhookController.handleWebhook);
+router.post('/webhook', webhookController.handleWebhook);
 router.post('/check', webhookController.handlecheck);
 
 module.exports = router;
