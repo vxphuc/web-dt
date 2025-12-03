f81a612af61ba8900c9b66f87acbe9359e0f1781
Phungthevinh <ptvstar2003@gmail.com>
2025-12-03 13:32:47 +0700

    Update apiCasso.js

--- utils/casso/apiCasso.js
+++ utils/casso/apiCasso.js
@@ -4,13 +4,16 @@ const queryString =  require("qs");
 require("dotenv").config();
 const api_key = process.env.API_KEY_CASSO;
 
+
+
 const axiosClient = axios.create({
   baseURL: 'https://oauth.casso.vn/v2',
   headers: {
     "content-type": "application/json",
-    "Authorization": `Apikey ${api_key}`,
+    "Authorization": `Apikey AK_CS.b8e1fdf0cf5a11f0a73fcb966f33aa53.M35e5gK4Xp6nl9Pz24kKyJl9lknCNVViYgS1IM8RQK568OPpEirhHd8ruE7jxzqBAanGyLhM`,
   },
   paramsSerializer: (params) => queryString.stringify(params),
+  
 });
 axiosClient.interceptors.request.use(async (config) => {
   return config;
