7c86156b7c3f59e7b816a6dd9c6ef1b8c41b40aa
Phungthevinh <ptvstar2003@gmail.com>
2025-12-15 08:31:18 +0700

    Update index.js

--- config/db/index.js
+++ config/db/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
+
+const connect_mongodb = process.env.CONNECT_MONGODB
 function connect() {
-    mongoose.connect('mongodb+srv://hkmediadtgroup:hkmedia@dtshop.ryjmkoe.mongodb.net/?retryWrites=true&w=majority&appName=DTShop')
+    mongoose.connect(connect_mongodb)
         .then(() => console.log('Connected!'));
 }
 // mongodb+srv://hkmediadtgroup:41iofkP4SrmA49wk@cluster0.3lrfdio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
