91e58f52cd803df62cb29709a0e6d59ba9e7a09a
Phungthevinh <ptvstar2003@gmail.com>
2025-12-20 09:41:13 +0700

    Update ipRateLimit.js

--- app/middlewares/rateLimit/ipRateLimit.js
+++ app/middlewares/rateLimit/ipRateLimit.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 //gia hạn số điện thoại request
 const limitPhone = rateLimit.rateLimit({
     windowMs: 5 * 60 * 1500,
-    limit: 10,
+    limit: 5,
     ipv6Subnet: 64,
     message: 'vui lòng đợi 5 phút rồi thử lại...',
 })
