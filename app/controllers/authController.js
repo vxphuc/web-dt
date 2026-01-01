b8c4384b1e9a2fc1ef2c754efe66e3fba4703072
Phungthevinh <ptvstar2003@gmail.com>
2026-01-01 19:28:21 +0700

    Update authController.js

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -119,7 +119,7 @@ async function signin(req, res, next) {
       await user.save();
     }
     // Tạo JWT token
-    const token = jwt.sign( {numberPhone : user.numberPhone, OTP: otp, role: user.role}, process.env.JWT_SECRET, { expiresIn: '8h' });
+    const token = jwt.sign( {numberPhone : user.numberPhone, OTP: otp, role: user.role}, process.env.JWT_SECRET, {algorithm: "HS256"} ,{ expiresIn: '8h' }, );
 
     res.json({ message: "Login successful", token });
   } catch(err) {
