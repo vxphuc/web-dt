a496a49c0cd14a76f80d1c7d38787f16beea53f7
vxphuc <phuc2652003@gmail.com>
2026-06-26 16:03:36 +0700

    update

--- routers/index.js
+++ routers/index.js
@@ -10,7 +10,7 @@ const webhookRouter = require('./webhook')
 const chartRouter = require('./Chart')
 const chatbotRouter = require('./chatbot')
 const Discount_code = require('./Discount_code')
-// const brochureRouter = require('./brochure') // TODO: Fix uploadPDF module not found
+const brochureRouter = require('./brochure') // TODO: Fix uploadPDF module not found
 
 
 function router(app){
@@ -25,7 +25,7 @@ function router(app){
     app.use('/chart', chartRouter)
     app.use('/chatbot', chatbotRouter)
     app.use('/discount-code', Discount_code)
-    // app.use('/brochure', brochureRouter) // TODO: Fix uploadPDF module not found
+    app.use('/brochure', brochureRouter) // TODO: Fix uploadPDF module not found
 }
 
 module.exports = router;
