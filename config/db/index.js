235f5c02de38698c83eadf8ef4dfb58607366876
vxphuc <phuc2652003@gmail.com>
2026-08-13 09:32:11 +0700

    Clean repository before public release

--- config/db/index.js
+++ config/db/index.js
@@ -6,6 +6,4 @@ function connect() {
     mongoose.connect(connect_mongodb)
         .then(() => console.log('Connected!'));
 }
-// mongodb+srv://hkmediadtgroup:41iofkP4SrmA49wk@cluster0.3lrfdio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
 module.exports = { connect };
